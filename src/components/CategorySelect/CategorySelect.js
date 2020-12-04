784eac50fcb80267ea0a22c1f897452e60b7142d
Jackson Tarlin <jacksontarlin@gmail.com>
2020-12-04 14:39:58 -0500

    added .svgs to main page

--- src/components/CategorySelect/CategorySelect.js
+++ src/components/CategorySelect/CategorySelect.js
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 import "./CategorySelect.scss";
+import asset1 from "../../assets/bx_bx-unite.svg";
+import asset2 from "../../assets/si-glyph_money-coin.svg";
+import asset3 from "../../assets/uil_file-graph.svg";
+import asset4 from "../../assets/Vector.svg";
 
 export default function CategorySelect() {
     return (
@@ -7,21 +11,25 @@ export default function CategorySelect() {
         <Link to="/pagetwo" style={{textDecoration: "none"}}> 
             <div className="card">
                 <span>Sales </span>
+                <img src={asset4} />
             </div>
         </Link>
         <Link to="/pagetwo" style={{textDecoration: "none"}}>
             <div className="card">
                 <span>Units Sold </span>
+                <img src={asset1} />
             </div>
         </Link>
         <Link to="/pagetwo" style={{textDecoration: "none"}}>
             <div className="card">
                 <span>Conversions </span>
+                <img src={asset2} />
             </div>
         </Link>
         <Link to="/pagetwo" style={{textDecoration: "none"}}>
             <div className="card">
                 <span>KPI Track </span>
+                <img src={asset3} />
             </div>
         </Link>
       </div>
