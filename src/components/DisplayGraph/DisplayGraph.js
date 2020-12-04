482d8779a369521a100afe62e78be49a2c5d33ee
Jackson Tarlin <jacksontarlin@gmail.com>
2020-12-04 14:54:49 -0500

    done

---
+++ src/components/DisplayGraph/DisplayGraph.js
@@ -0,0 +1,12 @@
+import asset1 from "../../assets/bx_bx-unite.svg";
+import asset2 from "../../assets/si-glyph_money-coin.svg";
+import asset3 from "../../assets/uil_file-graph.svg";
+import asset4 from "../../assets/Vector.svg";
+
+export default function CategorySelect() {
+    return (
+      <div className="cardContainer" style={{width: "100%"}}> 
+        
+      </div>
+    );
+  }
--- src/components/PageTwo.js
+++ src/components/PageTwo.js
@@ -2,17 +2,22 @@ import { Link } from "react-router-dom";
 import Header from "./Header";
 import SearchSort from "./SearchSort/SearchSort";
 import SortBy from "./SortBy/SortBy";
+import Aside from "./Aside";
 // import DataGraphs from
+import graphs from "../assets/graphs.png";
 
 function PageTwo() {
   return (
     <span>
       <Header />
       <div style={{ display: "flex" }}>
-        <div></div>
-        <div style={{ width: "100%" }}>
+        <div style={{width: "25%"}}>
+          <Aside/>
+        </div>
+        <div style={{width: "75%"}}>
           <SearchSort />
           <SortBy />
+          <img src={graphs} />
           {/* <DataGraphs /> */}
         </div>
       </div>
